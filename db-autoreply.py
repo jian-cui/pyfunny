384895df9de31ea9531918a80e6993520674b411
jian.cui <acui145@gmail.com>
2014-05-02 16:11:12 -0400

    1

--- db-autoreply.py
+++ db-autoreply.py
@@ -1,5 +1,27 @@
 from HTMLParser import HTMLParser
+import urllib
+import re
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        url = []
+        try:
+            if tag=='a' and attrs['class']=='title':
+                print attrs['title']
+                # url.append(attrs['title'])
+                reply_comment() !!!!!!!!!!!!!!!!!!!!!!!!
+        except:
+            pass
+    def handle_endtag(self, tag):
+        if tag=='html':
+            self.close()
+    def handle_data(self, data):
+        pass
+class Autoreply:
+    def __init(self):
+        pass
+    def reply(self, text):
+        
 
-f = open('temp.html')
-for i in readlines(f):
-    
+while i:
+    parser = MyHTMLParser()
+    parser.feed(htmlthing)
